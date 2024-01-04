954af7d78a1f65c2205f5bb753718154dc5ab735
musasina <yutubems@gmail.com>
2024-01-05 02:17:22 +0300

    Finished project

--- fixed-point.py
+++ fixed-point.py
@@ -1,13 +1,87 @@
+from collections.abc import Callable, Iterable, Mapping
+from typing import Any
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import jit
-from sympy import Function,Symbol
+from sympy import Function,Symbol,symbols, diff, solve, sympify, nroots,solve, Eq
 from sympy.parsing.sympy_parser import parse_expr
-from sympy.parsing.sym_expr import SymPyExpression
+from sympy.polys.polyerrors import PolynomialError
 from sympy.parsing.latex import parse_latex
 from scipy.optimize import minimize
+from threading import Barrier, Thread
 
-def fixed_point_method(func_repr : str, x0 : float, max_iter : int =100, tolerance : float=1e-6):
+def take_diff(expr):
+    x = Symbol("x")
+    return diff(expr, x) , x
+
+def find_extremums(expr)->list:
+    first_derivative , x =  take_diff(expr)
+    try:
+        extremums : list = sorted(nroots(first_derivative,n=6))
+        return extremums
+    except PolynomialError:
+        return [float(first_derivative)]
+
+def find_lims(expr, extremums: list,  tolerance : float=1e-6)-> list:
+    lims: list = [(expr.evalf(subs={"x": extremum - tolerance}),expr.evalf(subs={"x": extremum - 2*tolerance})) for extremum in extremums]
+    lims.append((expr.evalf(subs={"x": extremums[-1] + tolerance}),expr.evalf(subs={"x": extremums[-1] + 2*tolerance})))
+    return lims
+
+def find_signs(lims:list)->list:
+    print(lims)
+    signs = ["+" if e2>e and e2>0 else "-" if e2>e and e2<0 else "+" if e2<e and e2<0 else "-" for e,e2 in lims[:-1]]
+    signs.append(*["+" if e2>e and e2>0 else "+" if e2>e and e2<0 else "-" if e2<e and e2<0 else "+" for e,e2 in lims[-1:]])
+    return signs
+
+def make_expression(func_repr : str) -> object:
+    return parse_expr(func_repr)
+
+def find_extremums_first_diff(expr)-> list:
+    second_derivative , x = take_diff(take_diff(expr)[0])
+    try:
+        extremums = sorted(nroots(second_derivative,n=6))
+        extremums = extremums if extremums != [] else [-second_derivative.evalf(subs={"x": 0})]
+        return extremums
+    except PolynomialError:
+        return [float(second_derivative)]
+
+def find_up_down(lims: list):
+    directions = ["u" if (e2-e)<0 else "d" for e2,e in lims]
+    return directions
+
+def check_opposite_sign(signs : list)-> bool:
+    match signs[0]:
+        case "+":
+            return "-" in set(signs[1:])
+        case "-":
+            return "+" in set(signs[1:])
+        case _ :
+            raise NoAssumption("Wrong Sign")
+
+def check_opposite_direction(directions:list):
+    if directions != []:
+        match directions[0]:
+            case "u":
+                return "d" in set(directions[1:])
+            case "d":
+                return "u" in set(directions[1:])
+            case _ :
+                raise NoAssumption("Wrong Sign")
+    return True
+
+def make_assumption(extremums : list, directions : list, signs : list)->list:
+
+    points = []
+    if all([True if sign == "+" else False for sign in signs]) or all([True if sign == "-" else False for sign in signs]):
+        return points
+    for i, extremum in enumerate(extremums):
+        if check_opposite_direction(directions[i:]) and check_opposite_sign(signs[i:]):
+            points.append(extremum)
+    
+    return points
+
+@jit(forceobj=True,nogil=True)
+def fixed_point_method(expr,g_x,x0 : float,max_iter : int =100, tolerance : float=1e-6):
     """
     Find the root of a function using the fixed-point method.
 
@@ -23,18 +97,18 @@ def fixed_point_method(func_repr : str, x0 : float, max_iter : int =100, toleran
     """
     result : list = []
     iterations : int = 0
-    g_x = parse_expr(func_repr + "+ x")
-    while max_iter > iterations:
-        if x0 > -tolerance and x0 < tolerance:
-            break
-        x_new : float = g_x.evalf(subs={"x": x0})
-        if x0 == x_new:
-            break
-        x0 = x_new
+    error_1 : float = 1.0
+    error_2 : float = 1.0
+
+    while max_iter > iterations and error_1 > tolerance and error_2 > tolerance:
+
+        x_new : float = g_x.evalf(subs={"y": x0})
+        error_1 = abs(x_new-x0)
+        error_2 = abs(expr.evalf(subs={"x": x_new}))
         result.append(x0)
-        print(x0)
+        x0 = x_new
         iterations += 1
-        
+    
     return result, iterations
 
     # Plotting code
@@ -75,19 +149,56 @@ def plot_convergence(iterations, roots, true_root=None, title='Convergence Plot'
         ax2.set_ylabel('# Iterations')
         plt.show()
     
+class NoAssumption(RuntimeError):
+    pass
 
+class ReturnThread(Thread):
+    def __init__(self, group: None = None, target: Callable[..., object] | None = None, name: str | None = None, args: Iterable[Any] = ..., kwargs: Mapping[str, Any] | None = None, *, daemon: bool | None = None) -> None:
+        super().__init__(target=target,args=args)
+        self.result = None
+        self.target : callable = target
+        self.args : Iterable[Any] = args
+    
+    def run(self)->None:
+        self.result = self.target(*self.args)
 
+def make_g_x(expr):
+    x = Symbol("x")
+    y = Symbol("y")
+    eq = Eq(expr,y)
+    return solve(eq,x)
 
+def main(func_repr : str, max_iter : int =500, tolerance : float=1e-6):
 
-# Function to find the root of
-def f(x):
-    return x**3 - 3*x
+    expr = make_expression(func_repr=func_repr)
+    g_x = make_expression(func_repr=func_repr+"+ x")
+    g_x : list = make_g_x(g_x)
+    extremum_first_diff : list = find_extremums_first_diff(expr)
+    extremums : list = find_extremums(expr)
+    lims : list = find_lims(expr,extremums,tolerance)
+    signs : list = find_signs(lims)
+    lims_second_diff : list = find_lims(expr,extremum_first_diff,tolerance)
+    directions : list = find_up_down(lims_second_diff)
+    assumptions : list = make_assumption(extremums,directions,signs)
+    assumptions = assumptions if assumptions != [] else extremums
+    if assumptions == []:
+        raise NoAssumption("Starting point did not be found, Use different function")
+    dicts_1 : list = [{"target":fixed_point_method,"args":(expr,g,assumption-0.1,max_iter,tolerance)} for assumption in assumptions for g in g_x]
+    dicts_2 : list = [{"target":fixed_point_method,"args":(expr,g,assumption + 0.1,max_iter,tolerance)} for assumption in assumptions for g in g_x]
+    dicts_1.extend(dicts_2)
+    threads : list = [ReturnThread(target=dict_["target"],args=dict_["args"]) for dict_ in dicts_1]
 
-# Initial guess
-x0 = 1.5
+    for thread in threads:
+        thread.start()
 
-# Find the root using the fixed-point method
-roots, iterations = fixed_point_method("x**3.0 - 3.0*x", x0)
+    for thread in threads:
+        thread.join()
+    
+    results : list = [thread.result for thread in threads]
 
-print(f"Root: {roots[-1]}")
-print(f"Iterations: {iterations}")
+    return results
+
+if __name__ == "__main__":
+
+    # Find the root using the fixed-point method
+    print(main("x**2 - 2"))
