bdd03aebbd6c3c84105f977de08f2b012d99c5b0
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2024-01-05 10:34:56 +0300

    static files moved to static folders

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask,request,jsonify,render_template
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/calculate", methods=['POST'])
+def calculate():
+    try:
+        data = request.get_json()
+        decrypted_data = decrypt_data(data["input"])
+        return jsonify({"result": data}), 200
+    except Exception:
+        return jsonify({"error": "Invalid payload"}), 400
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -3,23 +3,24 @@ func.addEventListener("input", (event) => {
     func_value.innerHTML = event.target.value;
 });
 
-let size = document.getElementById("size");
-size.addEventListener("input", (event) => {
-    size_value.innerHTML = event.target.value;
+let x0 = document.getElementById("x0");
+x0.addEventListener("input", (event) => {
+    x0_value.innerHTML = event.target.value;
 });
+
 let max_iter = document.getElementById("max_iter");
 max_iter.addEventListener("input", (event) => {
     max_iter_value.innerHTML = event.target.value;
 });
+
 let tolerance = document.getElementById("tolerance");
 tolerance.addEventListener("input", (event) => {
     tolerance_value.innerHTML = event.target.value;
 });
 
-
 function fetchData() {
     let func = document.getElementById("Function").value;
-    let size = document.getElementById("size").value;
+    let x0 = document.getElementById("x0").value;
     let max_iter = document.getElementById("max_iter").value;
     let tolerance = document.getElementById("tolerance").value;
 
@@ -29,18 +30,18 @@ function fetchData() {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-                    digits: digits, 
-                    size: size,
-                    max_iter: max_iter,
-                    tolerance:tolerance
+            func: func,
+            x0: x0,
+            max_iter: max_iter,
+            tolerance: tolerance
         }),
     })
     .then(response => response.json())
     .then(data => {
-        plotResults(data);
+        // Handle the response data here or modify the sendJsontoPython function
+        console.log("Response data:", data);
     })
     .catch(error => {
         console.error("Error fetching data:", error);
     });
 }
-
